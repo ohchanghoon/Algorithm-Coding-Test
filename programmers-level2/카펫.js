b25354367998abfe67bd55930b6cbeae11a6c331
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-08 23:58:27 +0900

    ohchanghoon: 카펫 풀이(브루트포스,javascript)

---
+++ programmers-level2/카펫.js
@@ -0,0 +1,17 @@
+const brown = 10;
+const yellow = 2;
+
+function soulution(brown, yellow) {
+  for (let i = 0; i <= yellow; i += 1) {
+    if (yellow % i === 0) {
+      let h = i;
+      let w = yellow / h;
+
+      if (brown === w * 2 + w * 2 + 4) {
+        return [w + 2, h + 2];
+      }
+    }
+  }
+  return;
+}
+console.log(soulution(brown, yellow));
