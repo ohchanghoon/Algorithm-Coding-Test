20bc7f6cd48fe3a3eb7229425312e28fc2638069
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-07-13 23:42:51 +0900

    피보나치 수 5 풀이 (재귀, js)

--- BaekJoon/피보나치-수-5.js
+++ BaekJoon/피보나치-수-5.js
@@ -5,8 +5,9 @@
 let n = 10;
 
 function f(n) {
-  if (n === 0) return 0;
-  if (n === 1) return 1;
+  if (n === 0 || n === 1) return n;
+  // if (n === 0) return 0;
+  // if (n === 1) return 1;
   return f(n - 1) + f(n - 2);
 }
 console.log(f(n));
