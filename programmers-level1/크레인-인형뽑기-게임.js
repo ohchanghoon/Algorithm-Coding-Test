2a7fb0ad70507f895bc104628e9f18775baf5244
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-12-18 00:38:32 +0900

    Refactor: 크레인 인형뽑기 게임 풀이 (js)

--- programmers-level1/크레인-인형뽑기-게임.js
+++ programmers-level1/크레인-인형뽑기-게임.js
@@ -1,22 +1,41 @@
-function solution(board, moves) {
-  // const newMoves = moves.map((x) =>  x - 1);
-  let count = 0;
-  let stack = [];
+// function solution(board, moves) {
+//   let count = 0;
+//   let stack = [];
 
-  for (let i = 0; i < moves.length; i++) {
-    let now = moves[i] - 1;
-    for (let j = 0; j < board.length; j++) {
-      if (board[j][now] !== 0) {
-        if (stack[stack.length - 1] === board[j][now]) {
+//   for (let i = 0; i < moves.length; i++) {
+//     let now = moves[i] - 1;
+//     for (let j = 0; j < board.length; j++) {
+//       if (board[j][now] !== 0) {
+//         if (stack[stack.length - 1] === board[j][now]) {
+//           stack.pop();
+//           count += 2;
+//         } else {
+//           stack.push(board[j][now]);
+//         }
+//         board[j][now] = 0;
+//         break;
+//       }
+//     }
+//   }
+//   return count;
+// }
+function solution(board, moves) {
+  const stack = [];
+  let result = 0;
+
+  moves.forEach((move) => {
+    for (let i = 0; i < board.length; i += 1) {
+      if (board[i][move - 1]) {
+        if (stack[stack.length - 1] === board[i][move - 1]) {
           stack.pop();
-          count += 2;
+          result += 2;
         } else {
-          stack.push(board[j][now]);
+          stack.push(board[i][move - 1]);
         }
-        board[j][now] = 0;
+        board[i].splice(move - 1, 1, 0);
         break;
       }
     }
-  }
-  return count;
+  });
+  return result;
 }
