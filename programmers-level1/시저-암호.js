a9865d3d4f70186da1724ea87cc269063cbbb2cf
lovelyoch"ohchanghoon <dhckdgns3@naver.com>
2021-12-21 10:50:21 +0900

    Refactor: 시저 암호 풀이 (map, js)

--- programmers-level1/시저-암호.js
+++ programmers-level1/시저-암호.js
@@ -3,20 +3,34 @@ function solution(s, n) {
   const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let answer = '';
 
-  for (let i = 0; i < s.length; i += 1) {
-    const text = s[i];
-
-    if (text === ' ') {
+  s.split('').map((alphabet) => {
+    if (alphabet === ' ') {
       answer += ' ';
-      continue;
+      return;
     }
-    const textArr = lower.includes(text) ? lower : upper;
-    const pushedText = textArr.indexOf(text) + n;
 
-    answer +=
-      pushedText >= textArr.length
-        ? textArr[pushedText - textArr.length]
-        : textArr[pushedText];
-  }
+    const textArr = lower.includes(alphabet) ? lower : upper;
+    let idx = textArr.indexOf(alphabet) + n;
+
+    if (idx >= textArr.length) idx -= textArr.length;
+    answer += textArr[idx];
+  });
   return answer;
 }
+
+// for (let i = 0; i < s.length; i += 1) {
+//   const text = s[i];
+
+//   if (text === ' ') {
+//     answer += ' ';
+//     continue;
+//   }
+//   const textArr = lower.includes(text) ? lower : upper;
+//   const pushedText = textArr.indexOf(text) + n;
+
+//   answer +=
+//     pushedText >= textArr.length
+//       ? textArr[pushedText - textArr.length]
+//       : textArr[pushedText];
+// }
+// return answer;
